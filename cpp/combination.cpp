871ce7047fb499bf1d7cecde58565818997bc9b4
yell0wfever92 <mccarthydavid92@gmail.com>
2025-11-07 00:36:59 -0800

    add input validation

--- cpp/combination.cpp
+++ cpp/combination.cpp
@@ -10,7 +10,12 @@ int main() {
     cout << "Enter k: ";
     cin >> k;
     
-    // TODO: Validate input
+    // Validate that both n and k are greater than 0
+    if (n <= 0 || k <= 0) {
+        cout << "Result: -1" << endl;
+        return 0;
+    }
+    
     // TODO: Calculate combination
     
     return 0;
