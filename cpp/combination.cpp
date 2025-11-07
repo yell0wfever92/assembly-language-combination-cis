28fb5b841c589e8fa031cb6e59d71cfd65995299
yell0wfever92 <mccarthydavid92@gmail.com>
2025-11-07 00:20:54 -0800

    initial C++ skeleton

---
+++ cpp/combination.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    
+    // TODO: Get user input
+    // TODO: Validate input
+    // TODO: Calculate combination
+    
+    return 0;
+}
